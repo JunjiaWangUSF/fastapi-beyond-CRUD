3d66bef4352fc1b17ba86ec0634f80ce39f3f55e
Junjia Wang <jwang268@dons.usfca.edu>
2025-02-18 14:32:49 -0500

    debug email sender

--- scripts/sendEmail.js
+++ scripts/sendEmail.js
@@ -1,12 +1,7 @@
-const dotenv = require("dotenv");
-const path = require("path");
-
-// Set the path to the .env file relative to the current script
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
-
 const nodemailer = require("nodemailer");
 
-console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_USER); // Debug: Check the user value
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
@@ -25,9 +20,7 @@ const mailOptions = {
 transporter.sendMail(mailOptions, (error, info) => {
   if (error) {
     console.error("Error occurred: ", error.message);
-    // Handle errors appropriately in your context, e.g., logging or sending a response
   } else {
     console.log("Email sent: ", info.response);
-    // Handle successful send appropriately
   }
 });
